5fe54a8fc7bc86223098771cbc8f06f5d6fd62f2
Jim Newton <jnewton@node9.lrde.epita.fr>
2018-05-18 11:03:59 +0200

    changed ref to bin directory

--- bin/depend.sh
+++ bin/depend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+BIN="$HOME/sw/regular-type-expression/bin/"
 x=""
-x=$x:`qsub -l walltime=30 $HOME/bin/print-error.sh`
-qsub -l walltime=30 -W depend=afterok$x $HOME/bin/print-message.sh
+x=$x:`qsub -l walltime=30 $BIN/print-error.sh`
+qsub -l walltime=30 -W depend=afterok$x $BIN/print-message.sh
--- bin/make-reports.sh
+++ bin/make-reports.sh
@@ -6,19 +6,20 @@ export CLUSTER_JOB_NUM=$$
 mkdir cluster.$$
 cd cluster.$$
 echo starting jobs in cluster.$$
+BIN="$HOME/sw/regular-type-expression/bin/"
 
 x=""
 # there are 10 bucket-reports (0..9) and 11 decomposition functions
 for bucket in $(seq 0 9) ; do
-    x=$x:`qsub -l walltime=20000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $HOME/bin/big-report.lisp`
-    x=$x:`qsub -l walltime=10000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $HOME/bin/best-report.lisp`
-    x=$x:`qsub -l walltime=16000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $HOME/bin/parameterization-report.lisp`
-    x=$x:`qsub -l walltime=16000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $HOME/bin/bdd-report.lisp`
+    x=$x:`qsub -l walltime=20000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/big-report.lisp`
+    x=$x:`qsub -l walltime=10000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/best-report.lisp`
+    x=$x:`qsub -l walltime=16000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/parameterization-report.lisp`
+    x=$x:`qsub -l walltime=16000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/bdd-report.lisp`
     for decompose in $(seq 0 10) ; do
-	x=$x:`qsub -q lrde -l walltime=20000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="$bucket"  $HOME/bin/bdd-report-profile.lisp`
+	x=$x:`qsub -q lrde -l walltime=20000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="$bucket"  $BIN/bdd-report-profile.lisp`
     done
 done
 
-qsub -W depend=afterok$x -l walltime=1:00:00 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM" $HOME/bin/copy-reports
+qsub -W depend=afterok$x -l walltime=1:00:00 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM" $BIN/copy-reports
 
 

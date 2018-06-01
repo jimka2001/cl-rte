22aea8d3755411ec21821d50c13db1bba0a46393
Jim Newton <jnewton@lrde.epita.fr>
2018-06-01 14:48:16 +0200

    added copy of bdd-size log files as post-process

--- bin/copy-reports.sh
+++ bin/copy-reports.sh
@@ -2,3 +2,5 @@
 #PBS -m bea
 
 scp -r /lrde/home/jnewton/analysis/. johan:/Users/jnewton/analysis/.
+cat /lrde/home/jnewton/cluster.*/bdd-sizes.* | sort -u > /lrde/home/jnewton/bdd-sizes-unique
+scp /lrde/home/jnewton/bdd-sizes-unique johan:/Users/jnewton/analysis/bdd-sizes-unique

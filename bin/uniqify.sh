36df4b8a85e5c9706cfa797ff40924b5952b2890
Jim Newton <jnewton@lrde.epita.fr>
2018-06-26 15:26:32 +0200

    uniquify

--- bin/uniqify.sh
+++ bin/uniqify.sh
@@ -3,8 +3,14 @@
 cd /lrde/cluster/jnewton/bdd-sizes
 rm -ftmp
 for file in * ; do
+    echo -n before " " 
+    wc -l $file
     mv $file tmp
     cat tmp | sort -u > $file
+    echo -n after "  " 
+    wc -l $file
     git add $file
+    cat $file | sed -e 's/ [^ ]*$//' > $file.2-columns
+    git add $file.2-columns
 done
 git commit -m'automatic update'

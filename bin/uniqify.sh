30a2709d8d60bf8102509a96bfec81352d071f32
Jim Newton <jnewton@lrde.epita.fr>
2018-07-05 12:36:49 +0200

    updated uniqify.sh to only run one the .new files

--- bin/uniqify.sh
+++ bin/uniqify.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
+
 cd /lrde/cluster/jnewton/bdd-sizes
 rm -f tmp
-for file in bdd-sizes-unique-[0-9]? ; do
-    if [ -f $file.new ] ; then
-        mv $file.new tmp
-        sort -u -T $PWD tmp > $file.new
-        mv $file tmp
-        sort -m -u -T $PWD tmp $file.new > $file
-        rm tmp $file.new
-        wc -l $file
-        git add $file
-        cat $file | sed -e 's/ [^ ]*$/  -1/' > $file.2-columns
-        git add $file.2-columns
-        scp $file.2-columns johan:/Users/jnewton/analysis/.
-    fi
+
+for new in *new ; do
+    file="${new%%.*}"
+    touch $file
+    mv $new tmp
+    sort -u -T $PWD tmp > $new
+    mv $file tmp
+    sort -m -u -T $PWD tmp $new > $file
+    rm tmp $new
+    wc -l $file
+   cat $file | sed -e 's/ [^ ]*$/  -1/' > $file.2-columns
+   scp $file.2-columns johan:/Users/jnewton/analysis/.
 done
-rm -f tmp
-git commit -q -m'automatic update'

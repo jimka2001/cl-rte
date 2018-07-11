b1714ffd8673d92349e26540e117cc543051ca6b
Jim Newton <jnewton@lrde.epita.fr>
2018-07-11 16:02:41 +0200

    don't copy back to johan

--- bin/copy-reports.sh
+++ bin/copy-reports.sh
@@ -2,5 +2,4 @@
 #PBS -m bea
 
 scp -r /lrde/home/jnewton/analysis/. johan:/Users/jnewton/analysis/.
-cat /lrde/home/jnewton/cluster.*/bdd-sizes.* | sort -u > /lrde/home/jnewton/bdd-sizes-unique
-scp /lrde/home/jnewton/bdd-sizes-unique johan:/Users/jnewton/analysis/bdd-sizes-unique
+

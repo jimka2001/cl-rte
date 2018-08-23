84b83b88dcc473761e995600b7d5ddd5c6cdfd9a
Jim Newton <jnewton@lrde.epita.fr>
2018-08-23 14:05:40 +0200

    update number of decomposition functions to 14

--- bin/make-profile-reports.sh
+++ bin/make-profile-reports.sh
@@ -15,7 +15,7 @@ x=""
 # there are 10 bucket-reports (0..9) and 15 (0..14) decomposition functions
 # it is important that all the calls to mdtd-report-profile.lisp be done on the same architecture
 for bucket in $(seq 0 9) ; do
-    for decompose in $(seq 0 12) ; do
+    for decompose in $(seq 0 14) ; do
 	x=$x:`qsub -q lrde -l walltime=20000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="$bucket"  $BIN/mdtd-report-profile.lisp`
     done
 done

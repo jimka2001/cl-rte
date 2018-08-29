fdb46f768eb33b88299f8391ab05587a34fcceeb
Jim Newton <jnewton@lrde.epita.fr>
2018-08-29 14:53:42 +0200

    added baker script

---
+++ bin/baker-reports.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# qselect -u $USER -s RQ | xargs --no-run-if-empty qdel
+# cd /lrde/home/jnewton/sw/regular-type-expression ; git pull --no-edit
+cd
+export CLUSTER_JOB_NUM=$$
+if [ -d cluster.$$ ]; then
+  mv cluster.$$ "cluster.$$.`date`"
+fi
+mkdir cluster.$$
+cd cluster.$$
+echo starting jobs in cluster.$$
+BIN="$HOME/sw/regular-type-expression/bin/"
+
+x=""
+# there are 10 bucket-reports (0..9)
+for bucket in $(seq 0 9) ; do
+    x=$x:`qsub -l walltime=10000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/baker-report.lisp`
+    x=$x:`qsub -l walltime=16000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",BUCKET-INDEX="$bucket" $BIN/mdtd-report.lisp`
+done
+
+

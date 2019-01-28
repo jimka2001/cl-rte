4f92aec87b9d1bb35eceecf5e57c49ee35b64c6c
Jim Newton <jimka.issy@gmail.com>
2019-01-28 16:49:34 +0100

    debugging script

--- bin/test.sh
+++ bin/test.sh
@@ -11,7 +11,7 @@ echo starting jobs in cluster.$$
 BIN="$HOME/sw/regular-type-expression/bin/"
 
 
-qsub -q infinite -l walltime=80000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="0"  $BIN/test-lisp.lisp
+qsub -q infinite -l walltime=80000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="3",BUCKET-INDEX="7"  $BIN/test-lisp.lisp
 
 
 exit 0

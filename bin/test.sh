3f019d69c288bbe883ec60d3add129533e8608a4
Jim Newton <jimka.issy@gmail.com>
2019-01-28 16:46:52 +0100

    debugging shell script to launch lisp test

--- bin/test.sh
+++ bin/test.sh
@@ -10,7 +10,8 @@ cd cluster.$$
 echo starting jobs in cluster.$$
 BIN="$HOME/sw/regular-type-expression/bin/"
 
-qsub -q infinite -l walltime=80000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="$bucket"  $BIN/test-lisp.lisp
+
+qsub -q infinite -l walltime=80000 -v CLUSTER_JOB_NUM="$CLUSTER_JOB_NUM",DECOMPOSE-INDEX="$decompose",BUCKET-INDEX="0"  $BIN/test-lisp.lisp
 
 
 exit 0

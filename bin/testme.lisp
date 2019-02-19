5bca669bb672ea7edcaea6dcabb199decba08e3e
Jim Newton <jimka.issy@gmail.com>
2019-02-19 16:36:35 +0100

    adding quicklisp support to auto-test

--- bin/testme.lisp
+++ bin/testme.lisp
@@ -10,12 +10,13 @@
 ;; (declaim (optimize (safety 3) (debug 3) (space 0) (speed 0))) 
 (declaim (optimize (safety 1) (debug 0) (space 0) (speed 3) (compilation-speed 0)))
 
-#-quicklisp
 (let ((quicklisp-init
-	"/lrde/home/jnewton/quicklisp/setup.lisp"))
+	"ql/setup.lisp"))
   (if (probe-file quicklisp-init)
       (load quicklisp-init)
       (error "file not found ~S" quicklisp-init)))
+(setf sb-impl::*default-external-format* :utf-8)
+(setf sb-alien::*default-c-string-external-format* :utf-8)
 (asdf:load-system :research)
 (in-package :scrutiny)
 (run-tests)

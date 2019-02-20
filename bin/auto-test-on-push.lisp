7b98b5cb65ccb875f810b0d76b712e00f6f004d0
Jim Newton <jimka.issy@gmail.com>
2019-02-20 08:43:19 +0100

    debug

--- bin/auto-test-on-push.lisp
+++ bin/auto-test-on-push.lisp
@@ -10,14 +10,20 @@
 ;; (declaim (optimize (safety 3) (debug 3) (space 0) (speed 0))) 
 (declaim (optimize (safety 1) (debug 0) (space 0) (speed 3) (compilation-speed 0)))
 
-(format t "~A~%" (sb-posix:getcwd))
+(format t "cwd=~A~%" (sb-posix:getcwd))
+(format t "user-homedir=~A~%" (user-homedir-pathname))
 (let ((quicklisp-init
 	"/root/quicklisp/setup.lisp"))
   (if (probe-file quicklisp-init)
       (load quicklisp-init)
       (error "file not found ~S" quicklisp-init)))
 
-
+(asdf:initialize-source-registry '(:source-registry
+                                   (:tree ("/Users/jnewton/common-lisp/"))
+                                   (:tree (:home "src/common-lisp/"))
+                                   (:tree (:home "share/common-lisp/source/"))
+                                   :inherit-configuration
+                                   ))
 (setf sb-impl::*default-external-format* :utf-8)
 (setf sb-alien::*default-c-string-external-format* :utf-8)
 (asdf:load-system :research)

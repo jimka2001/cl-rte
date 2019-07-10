4266b47f33a9540502e4fa910dc469182cd92dbe
Jim Newton <jimka.issy@gmail.com>
2019-07-10 14:56:39 +0200

    debug

--- bin/dimacs-parse.sh
+++ bin/dimacs-parse.sh
@@ -10,6 +10,6 @@ initial=`qsub -l walltime=3000 $BIN/dimacs-compile.csh`
 cd $inDir
 for cnf in g2-ACG-*.cnf
 do
-  qsub -W depend=afterok:$initial $BIN/dimacs-parse-1.csh $inDir $cnf
+  qsub -W depend=afterok:$initial "$BIN/dimacs-parse-1.csh $inDir $cnf"
 done
 

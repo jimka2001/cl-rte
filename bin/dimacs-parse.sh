5b0147d089a45425e3d3808c1d2d849cb617bbcc
Jim Newton <jimka.issy@gmail.com>
2019-07-12 10:34:01 +0200

    treating files in RandomSat

--- bin/dimacs-parse.sh
+++ bin/dimacs-parse.sh
@@ -3,11 +3,25 @@
 set verbose echo
 
 BIN="$HOME/sw/regular-type-expression/bin/"
-inDir=/lrde/cluster/jnewton/SAT-benchmarks/NoLimits
-outDir=/lrde/cluster/jnewton/QM-reduce/NoLimits
 cd $BIN/..
 
 initial=`qsub -l walltime=3000 $BIN/dimacs-compile.csh`
+
+
+# inDir=/lrde/cluster/jnewton/SAT-benchmarks/NoLimits
+# outDir=/lrde/cluster/jnewton/QM-reduce/NoLimits
+
+# cd $inDir
+# for cnf in *.cnf
+# do
+#   echo file = $cnf
+#   cd $BIN/..
+#   qsub -W depend=afterok:$initial -l nodes=1:ppn=4 -F  "$inDir $outDir $cnf"  $BIN/dimacs-parse-1.csh
+# done
+
+
+inDir=/lrde/cluster/jnewton/SAT-benchmarks/RandomSat
+outDir=/lrde/cluster/jnewton/QM-reduce/RandomSat
 
 cd $inDir
 for cnf in *.cnf
